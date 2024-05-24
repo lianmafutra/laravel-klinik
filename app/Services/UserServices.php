767b3c1806778a0a9520b9dfb2287d2568e59314
lianmafutra <lianmafutra@gmail.com>
2024-05-24 18:51:11 +0700

    delete old master user

--- app/Services/UserServices.php
+++ app/Services/UserServices.php
@@ -1,13 +1,9 @@
 <?php
-
 namespace App\Services;
-
 class UserServices
 {
-
    public function getJenisUserColor($role)
    {
-
       $color = '';
       switch ($role) {
          case 'siswa':
@@ -17,26 +13,6 @@ class UserServices
             $color = '#3f51b5';
             break;
       }
-
-      return '<span style="color: #fff;  opacity: 0.6; background-color:' . $color . '"  class="badge">' . $role . '</span>';
-   }
-
-
-   public function getRoleColor($role)
-   {
-
-      // Seed the random number generator to ensure consistent colors for the same input string
-      srand(crc32($role));
-
-      // Generate random RGB values based on the characters in the string
-      $red = rand(0, 255);
-      $green = rand(0, 255);
-      $blue = rand(0, 255);
-
-      // Return the RGB values as a hexadecimal color code
-      $color = sprintf('#%02x%02x%02x', $red, $green, $blue);
-
-
       return '<span style="color: #fff;  opacity: 0.6; background-color:' . $color . '"  class="badge">' . $role . '</span>';
    }
 
@@ -47,5 +23,18 @@ class UserServices
       } else if ($jenis_user == "siswa") {
          return $nip;
       }
+   }
+
+   public function getRoleColor($role)
+   {
+      // // Seed the random number generator to ensure consistent colors for the same input string
+      // srand(crc32($role));
+      // // Generate random RGB values based on the characters in the string
+      // $red = rand(0, 255);
+      // $green = rand(0, 255);
+      // $blue = rand(0, 255);
+      // // Return the RGB values as a hexadecimal color code
+      // $color = sprintf('#%02x%02x%02x', $red, $green, $blue);
+      return '<span   class="badge badge badge-info">' . $role . '</span>';
    }
 }
