737325290eee971bf353643a8cecd0eff0209c08
lianmafutra <lianmafutra@gmail.com>
2024-05-26 11:04:18 +0700

    pemeriksaan struk cetak

--- resources/views/app/pemeriksaan/riwayat-cetak.blade.php
+++ resources/views/app/pemeriksaan/riwayat-cetak.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Praktik Dokter Umum dr. Tri Indriani</title>
+    <title>Klinik SAMPEYAN</title>
     <style>
         body {
             font-family: Arial, sans-serif;
