73eacea65b43cab4447e69c6083e241e4c143812
lianmafutra <lianmafutra@gmail.com>
2024-05-27 17:53:04 +0700

    fix pemeriksaan case sensitive name

--- app/Http/Controllers/Klinik/Pemeriksaan/PemeriksaanController.php
+++ app/Http/Controllers/Klinik/Pemeriksaan/PemeriksaanController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\klinik\Pemeriksaan;
+namespace App\Http\Controllers\Klinik\Pemeriksaan;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PemeriksaanRequest;
